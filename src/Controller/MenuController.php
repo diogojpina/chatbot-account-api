9de44c11f8eb3c63f99db77d0334fa54c92bb6ee
Diogo Pina <diogojpina@gmail.com>
2020-05-08 00:09:39 -0300

    Adding chose menu item endpoint at Menu controller

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -7,26 +7,40 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-
-use App\Service\Authentication;
-
-
 class MenuController extends ApiController {
 
-	private function getMenuOptions() {
-		$menu = array();
-		$menu[1] = 'Deposit';
-		$menu[2] = 'Withdraw';
-		$menu[3] = 'Balance';
+	private function getOptions() {
+		$options = array();
+		$options[1] = array('name' => 'Deposit', 'code' => 'deposit');
+		$options[2] = array('name' => 'Withdraw', 'code' => 'withdraw');
+		$options[3] = array('name' => 'Balance', 'code' => 'balance');
 
-		return $menu;
+		return $options;
 	}
 
 	/**
 	* @Route("/menu", name="menu_list", methods={"GET"})
 	*/
 	public function list() {
-    	return $this->json(array('success' => true, 'data' => $this->getMenuOptions()));
+    	return $this->json(array('success' => true, 'data' => $this->getOptions()));
+	}
+
+	/**
+	* @Route("/menu/chooseOption", name="menu_choose_option", methods={"POST"})
+	*/
+	public function chooseOption(Request $request) {
+		$request = $this->transformJsonBody($request);
+		$option = $request->request->get('option', '');
+		$options = $this->getOptions();
+
+		foreach ($options as $key => $optionValue) {
+			if ($key == $option) {
+				return $this->json(['success' => true, $key => $optionValue]);
+			}
+		}
+
+
+		return $this->json(['success' => false, 'message' => 'Option not found!']);
 	}
 
 
