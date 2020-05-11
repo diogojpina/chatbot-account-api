4e85b1db5cc55ea291a7ddc2bd1165b920d8726f
Diogo Pina <diogojpina@gmail.com>
2020-05-11 12:52:22 -0300

    Updating unit tests

--- tests/Controller/AuthenticationControllerTest.php
+++ tests/Controller/AuthenticationControllerTest.php
@@ -7,29 +7,33 @@ use App\Controller\AuthenticationController;
 use App\Service\Authentication;
 
 class AuthenticationControllerTest extends TestCase {
+	private $user;
+
+	protected function setUp() {		
+		$this->user = new \App\Entity\User();
+		$this->user->setId(1);
+		$this->user->setUsername('user');
+		$this->user->setPassword('pass');
+		$this->user->setToken('token');
+	}
+
 	public function testLogin() {
-
-		/*
 		$request = $this->createMock(\Symfony\Component\HttpFoundation\Request::class);
-
 		$request->request = $this->createMock(\Symfony\Component\HttpFoundation\ParameterBag::class);
 
+		$request->request
+			->method('get')
+			->willReturn('123');
 
 		$authentication = $this->createMock(\App\Service\Authentication::class);
-
-		$user = new \App\Entity\User();
-
-
 		
 		$authentication
-			->expects($this->atLeastOnce())
 			->method('login')
-			->willReturn($user);
-		*/
+			->willReturn(null);
 		
 
-		//$controller = new AuthenticationController();
-		//$controller->login($authentication, $request);
+		$controller = new AuthenticationController();
+		$controller->login($authentication, $request);
 		
 		$this->assertEquals(42, 42);
 	}
--- tests/Service/AuthenticationTest.php
+++ tests/Service/AuthenticationTest.php
@@ -7,16 +7,11 @@ class AuthenticationTest extends TestCase {
 	private $user;
 
 	protected function setUp() {		
-
 		$this->user = new \App\Entity\User();
 		$this->user->setId(1);
 		$this->user->setUsername('user');
 		$this->user->setPassword('pass');
 		$this->user->setToken('token');
-
-		
-
-		
 	}
 
 	public function testLogin() {
